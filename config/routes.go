44412eba15b0ba28faa6d279b42e0b83cfe67ecc
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-07 08:52:44 +0900

    remove ping request to routes.go

--- config/routes.go
+++ config/routes.go
@@ -10,6 +10,11 @@ func InitRoutes() *gin.Engine {
 
 	missionController := controller.MissionController{}
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.GET("/missions", missionController.GetAllMissions)
 
 	return r
