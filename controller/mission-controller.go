d10ac1548a17fa008b20cc60578a52f96a89f6d0
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-09 08:47:57 +0900

    test request handling with mock Mission data

--- controller/mission-controller.go
+++ controller/mission-controller.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"hr-app-go/model"
+)
 
 type MissionRequestHandler interface {
 	GetAllMissions(*gin.Context)
@@ -11,8 +14,14 @@ type MissionController struct {
 }
 
 func (controller *MissionController) GetAllMissions(c *gin.Context) {
+	// TODO get Missions from DB
+	missions := []model.Mission{
+		{Statement: "人々を技術で前進させる"},
+		{Statement: "生き物への慈愛の念を広める"},
+	}
 	c.JSON(200, gin.H{
-		"message": "DEBUG: GetAllMissions has been called!",
+		"message":  "DEBUG: GetAllMissions has been called!",
+		"missions": missions,
 	})
 }
 
