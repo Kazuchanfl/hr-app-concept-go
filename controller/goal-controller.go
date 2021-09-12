de7a61e8e96c588d314a247e36747eeabbe8c665
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-12 10:19:36 +0900

    add goal controller struct

--- controller/goal-controller.go
+++ controller/goal-controller.go
@@ -1,1 +1,11 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+type GoalRequestHandler interface {
+	GetAllGoals(*gin.Context)
+	GetAGoal(*gin.Context)
+}
+
+type GoalController struct {
+}
