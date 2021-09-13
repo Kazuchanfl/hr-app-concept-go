4e205248ac69c060faf28b8a73c81d1eaa6b1b41
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-14 08:47:57 +0900

    implement GetAllGoals

--- controller/goal-controller.go
+++ controller/goal-controller.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"hr-app-go/model"
+)
 
 type GoalRequestHandler interface {
 	GetAllGoals(*gin.Context)
@@ -8,4 +11,16 @@ type GoalRequestHandler interface {
 }
 
 type GoalController struct {
+}
+
+func (controller *GoalController) GetAllGoals(c *gin.Context) {
+	// TODO get Goals from DB
+	goals := []model.Goal{
+		{Title: "頑張る", Description: "めっちゃ頑張る"},
+		{Title: "もっと頑張る", Description: "すごい頑張る"},
+	}
+	c.JSON(200, gin.H{
+		"message": "DEBUG: GetAllGoals has been called!",
+		"goals":   goals,
+	})
 }
