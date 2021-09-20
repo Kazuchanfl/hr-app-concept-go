650604208a490f46a466793b6d08811a7f3cc071
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-20 11:16:59 +0900

    add mock Mission Statement

--- repository/mission-repository.go
+++ repository/mission-repository.go
@@ -12,6 +12,7 @@ type MissionRepository struct {
 func (r *MissionRepository) GetAllMissions() []model.Mission {
 	// TODO takahashikazuaki get Mission from DB
 	return []model.Mission{
-		{Statement: "人の成長を支えることを通じて世の中の成長を実現する"},
+		{Statement: "人の成長を支えることを通じて世の中の成長を実現する。"},
+		{Statement: "生き物への慈愛の念を広め、人と動物が共生できる社会を実現する。"},
 	}
 }
