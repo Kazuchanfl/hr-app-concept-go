8aabf299cb330ebed8fafa6d5e37f9b562c33463
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-20 11:02:09 +0900

    create GetAllMission method in repository

--- repository/mission-repository.go
+++ repository/mission-repository.go
@@ -1,4 +1,17 @@
 package repository
 
+import (
+	"gorm.io/gorm"
+	"hr-app-go/model"
+)
+
 type MissionRepository struct {
+	db *gorm.DB
+}
+
+func (r *MissionRepository) GetAllMissions() []model.Mission {
+	// TODO takahashikazuaki get Mission from DB
+	return []model.Mission{
+		{Statement: "人の成長を支えることを通じて世の中の成長を実現する"},
+	}
 }
