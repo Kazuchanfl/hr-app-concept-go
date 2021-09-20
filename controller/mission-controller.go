1b1f89bcfd6564befec4276be36c4a4fb6e6e29f
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-20 11:17:20 +0900

    call Controller's repository method

--- controller/mission-controller.go
+++ controller/mission-controller.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"hr-app-go/model"
 	"hr-app-go/repository"
 )
 
@@ -11,11 +10,7 @@ type MissionController struct {
 }
 
 func (controller *MissionController) GetAllMissions(c *gin.Context) {
-	// TODO get Missions from DB
-	missions := []model.Mission{
-		{Statement: "人々を技術で前進させる"},
-		{Statement: "生き物への慈愛の念を広める"},
-	}
+	missions := controller.Repository.GetAllMissions()
 	c.JSON(200, gin.H{
 		"message":  "DEBUG: GetAllMissions has been called!",
 		"missions": missions,
