949e899a017d492cd3e6ac8ce5ac31bc75b674f0
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-26 10:50:19 +0900

    get user properties from PostForm

--- controller/user-controller.go
+++ controller/user-controller.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"hr-app-go/model"
 	"hr-app-go/repository"
 )
 
@@ -26,11 +25,14 @@ func (co *UserController) GetAUser(c *gin.Context) {
 }
 
 func (co *UserController) UpdateAUser(c *gin.Context) {
-	// TODO takahashikazuaki call repository
-	//user, err := co.R.UpdateAUser(c.Param("id"))
-	user := model.User{Name: "Kazuaki Takahashi", Profile: "Graduate Student of Waseda University"}
+	// TODO takahashikazuaki handle error for failing GetAUser
+	user, _ := co.R.GetAUser(c.Param("id"))
+	user.Name = c.PostForm("name")
+	user.Profile = c.PostForm("profile")
+	updatedUser, _ := co.R.UpdateAUser(user)
+	//user := model.User{Name: "Kazuaki Takahashi", Profile: "Graduate Student of Waseda University"}
 	c.JSON(200, gin.H{
 		"message": "DEBUG: UpdateAUser has been called!",
-		"user":    user,
+		"user":    updatedUser,
 	})
 }
