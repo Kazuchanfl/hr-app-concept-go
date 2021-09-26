8231be31b446054afaefe94f9bbb5553f9695424
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-27 08:57:11 +0900

    add GetAGoal method

--- repository/goal-repository.go
+++ repository/goal-repository.go
@@ -1,7 +1,15 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"hr-app-go/model"
+)
 
 type GoalRepository struct {
 	DB *gorm.DB
+}
+
+func (r *GoalRepository) GetAGoal(id string) (model.Goal, error) {
+	// TODO takahashikazuaki not yet implemented
+	return model.Goal{}, nil
 }
