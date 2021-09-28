6b37eb7127d684ae2f8f7d74e89ae0a2b2c7957f
髙橋和晃 <kazuaki.flute@gmail.com>
2021-09-29 08:25:07 +0900

    add endpoint for getting all values

--- controller/value-controller.go
+++ controller/value-controller.go
@@ -1,4 +1,13 @@
 package controller
 
+import "github.com/gin-gonic/gin"
+
 type ValueController struct {
+}
+
+func (co *ValueController) GetAllValues(c *gin.Context) {
+	// TODO takahashikazuaki not yet implemetned
+	c.JSON(200, gin.H{
+		"message": "DEBUG: GetAllValues has been called!",
+	})
 }
