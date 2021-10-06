03cf10a8024db7ae6c6002df8ccce148f7559f8d
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-07 08:00:28 +0900

    get all values from db

--- repository/value-repository.go
+++ repository/value-repository.go
@@ -1,7 +1,17 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"hr-app-go/model"
+)
 
 type ValueRepository struct {
 	DB *gorm.DB
+}
+
+func (r *ValueRepository) GetAllValues() ([]model.Value, error) {
+	// TODO takahashikazuaki handle DB error
+	var values []model.Value
+	r.DB.Find(&values)
+	return values, nil
 }
