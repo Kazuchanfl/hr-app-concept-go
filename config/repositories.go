afc8e599a60a5675d7cadeefe1feafcc465462a6
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-09 07:52:16 +0900

    init repository with goal repository

--- config/repositories.go
+++ config/repositories.go
@@ -9,12 +9,14 @@ type Repositories struct {
 	MissionR *repository.MissionRepository
 	ValueR   *repository.ValueRepository
 	UserR    *repository.UserRepository
+	GoalR    *repository.GoalRepository
 }
 
 func InitRepositories(db *gorm.DB) Repositories {
 	missionR := &repository.MissionRepository{DB: db}
 	userR := &repository.UserRepository{DB: db}
 	valueR := &repository.ValueRepository{DB: db}
+	goalR := &repository.GoalRepository{DB: db}
 
-	return Repositories{MissionR: missionR, ValueR: valueR, UserR: userR}
+	return Repositories{MissionR: missionR, ValueR: valueR, UserR: userR, GoalR: goalR}
 }
