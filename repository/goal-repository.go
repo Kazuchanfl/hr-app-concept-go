1f67835cfd2e75b7876c654d14c2474d125e0576
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-11 08:29:05 +0900

    add repository method for getting all goals of a user

--- repository/goal-repository.go
+++ repository/goal-repository.go
@@ -17,3 +17,14 @@ func (r *GoalRepository) GetAGoal(id string) (model.Goal, error) {
 	}
 	return goal, nil
 }
+
+func (r *GoalRepository) GetGoalsOfAUser(userId string) ([]model.Goal, error) {
+	// TODO takahashikazuaki get goals from DB
+	goals := []model.Goal{
+		{
+			Title:       "メンバーと毎週定例を実施する",
+			Description: "その日やっていたこと・今後やっていこうとしていること・課題などを共有し、解決に向けて前進させる。",
+		},
+	}
+	return goals, nil
+}
