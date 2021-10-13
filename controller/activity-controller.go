71d6086b6d1cfb4489f96eeb7b139eb8b33fc5bd
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-14 08:58:46 +0900

    add controller method for creating an activity

--- controller/activity-controller.go
+++ controller/activity-controller.go
@@ -1,5 +1,14 @@
 package controller
 
+import "github.com/gin-gonic/gin"
+
 // TODO takahashikazuaki ActivityController should be included
 type ActivityController struct {
+}
+
+func (co *ActivityController) CreateAnActivity(c *gin.Context) {
+	// TODO takahashikazuaki store an activity in DB
+	c.JSON(200, gin.H{
+		"message": "DEBUG: CreateAnActivity has been called!",
+	})
 }
