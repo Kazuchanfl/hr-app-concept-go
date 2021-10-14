6a1177c698437ecd4c7250433e028f571fb091d8
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-15 08:46:44 +0900

    add repository method for creating an activity

--- repository/activity-repository.go
+++ repository/activity-repository.go
@@ -5,3 +5,8 @@ import "gorm.io/gorm"
 type ActivityRepository struct {
 	DB *gorm.DB
 }
+
+func (r *ActivityRepository) CreateAnActivity() error {
+	// TODO create an activity and store in DB
+	return nil
+}
