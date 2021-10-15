d1ba4b94ef2310a28a14cfa34e14c063c3b69f85
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-16 08:54:29 +0900

    implement creating logic

--- repository/activity-repository.go
+++ repository/activity-repository.go
@@ -1,12 +1,22 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"hr-app-go/model"
+)
 
 type ActivityRepository struct {
 	DB *gorm.DB
 }
 
+// TODO takahashikazuaki Get activity object and create record with it
 func (r *ActivityRepository) CreateAnActivity() error {
-	// TODO create an activity and store in DB
-	return nil
+	activity := model.Activity{
+		Title:       "1日に３つLGTMを出した",
+		Description: "レビューが必要なプルリクエストが多かったので３つ分レビューし、LGTMを出した。",
+		UserID:      1,
+		GoalID:      1,
+	}
+	result := r.DB.Create(&activity)
+	return result.Error
 }
