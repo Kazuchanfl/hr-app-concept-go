4112cc7c4f8e0e747f2f4afba3b2339bdfc37fd5
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-28 08:57:40 +0900

    let controller have repository

--- controller/activity-controller.go
+++ controller/activity-controller.go
@@ -1,13 +1,17 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"hr-app-go/repository"
+)
 
-// TODO takahashikazuaki ActivityController should be included
 type ActivityController struct {
+	R *repository.ActivityRepository
 }
 
 func (co *ActivityController) CreateAnActivity(c *gin.Context) {
-	// TODO takahashikazuaki store an activity in DB
+	// TODO takahashikazuaki create an activity instance and pass it to repository
+	_ = co.R.CreateAnActivity()
 	c.JSON(200, gin.H{
 		"message": "DEBUG: CreateAnActivity has been called!",
 	})
