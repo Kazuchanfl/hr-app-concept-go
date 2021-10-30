3e147c6be6f284232865f1ef11d7e9985ec4e5fc
髙橋和晃 <kazuaki.flute@gmail.com>
2021-10-30 10:02:57 +0900

    Get activity object and create record with it

--- repository/activity-repository.go
+++ repository/activity-repository.go
@@ -9,14 +9,7 @@ type ActivityRepository struct {
 	DB *gorm.DB
 }
 
-// TODO takahashikazuaki Get activity object and create record with it
-func (r *ActivityRepository) CreateAnActivity() error {
-	activity := model.Activity{
-		Title:       "1日に３つLGTMを出した",
-		Description: "レビューが必要なプルリクエストが多かったので３つ分レビューし、LGTMを出した。",
-		UserID:      1,
-		GoalID:      1,
-	}
+func (r *ActivityRepository) CreateAnActivity(activity *model.Activity) error {
 	result := r.DB.Create(&activity)
 	return result.Error
 }
